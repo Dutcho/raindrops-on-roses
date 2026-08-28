123154afcb2c76823f6fd73a11c1857733727643
Olaf <Dutcho@users.noreply.github.com>
2026-08-28 11:56:09 +0200

    Added cast_alike and rationalized cast_alike_callable in types

--- src/favs/_internal/types.py
+++ src/favs/_internal/types.py
@@ -1,34 +1,43 @@
 from collections.abc import Callable
-from typing import Any, assert_type, cast, overload, TYPE_CHECKING
-
-from typing_extensions import TypeForm
+from typing import assert_type, cast, TYPE_CHECKING
 
 
-@overload
-def cast_alike_callable[**PS, RT](func_type: TypeForm[Callable[PS, RT]], value: object, /) -> Callable[PS, RT]:
-    """Return `value` cast to `func_type`, i.e. to same type and signature."""
+def cast_alike[T](_exemplar: T, instance: object, /) -> T:
+    """Return `instance` cast alike `_exemplar`, i.e. to type `T`.
 
+    Equivalent to `cast(type(_exemplar), instance)`, which `mypy` 2.3.1 disapproves (though `ty` 0.0.73 allows).
 
-@overload
-def cast_alike_callable[**PS, RT](func: Callable[PS, RT], value: object, /) -> Callable[PS, RT]:
-    """Return `value` cast alike callable `func`, i.e. to same type and signature."""
-
-
-def cast_alike_callable[**PS, RT](_arg: Callable[PS, RT] | TypeForm[Callable[PS, RT]], value: object, /
-                                  ) -> Callable[PS, RT]:
-    """Return `value` cast alike callable `func` (or to `func_type` if `TypeForm`), i.e. to same type and signature.
-
-    >>> f = cast_alike_callable(getattr, lambda *args: 0)       # lie to typechecker about signature
-    >>> assert_type(f, Callable[[object, str], Any])            # cannot validate at runtime # doctest: +SKIP
+    >>> n = next(iter(range(1)))    # prevent typechecker from inferring `Literal`
+    >>> obj = cast_alike(n, 'abc')  # lie about `obj` type to typechecker
+    >>> assert_type(obj, int)       # cannot validate at doctest runtime # doctest: +SKIP
     """
-    if TYPE_CHECKING:
-        getattr_type = Callable[[object, str], Any]
-        f = cast_alike_callable(getattr, lambda *args: 0)       # redo doctest at typechecking time
-        assert_type(f, getattr_type)                            # typechecker believes lie about f
-        g = cast_alike_callable(getattr_type, lambda *args: 0)  # typeform test at typechecking time
-        assert_type(g, getattr_type)                            # typechecker believes lie about f
+    return cast(T, instance)
 
-    return cast(Callable[PS, RT], value)
+
+if TYPE_CHECKING:
+    def _test_cast_alike() -> None:
+        """Execute skipped doctest at typechecking time."""
+        n = next(iter(range(1)))
+        obj = cast_alike(n, 'abc')
+        assert_type(obj, int)       # typechecker believes lie about `obj` type
+
+
+def cast_alike_callable[**PS, RT](_exemplar: Callable[PS, RT], instance: object, /) -> Callable[PS, RT]:
+    """Return `instance` cast alike callable `_exemplar`, i.e. to same type and signature `Callable[PS, RT]`.
+
+    `cast_alike_callable` standardizes result type to parametrized `Callable` (for mypy), which `cast_alike` can't.
+
+    >>> func = cast_alike_callable(ascii, lambda a, b: 0)  # lie about `func` type and signature to typechecker
+    >>> assert_type(func, Callable[[object], str])         # cannot validate at doctest runtime # doctest: +SKIP
+    """
+    return cast(Callable[PS, RT], instance)
+
+
+if TYPE_CHECKING:
+    def _test_cast_alike_callable() -> None:
+        """Execute skipped doctest at typechecking time."""
+        func = cast_alike_callable(ascii, lambda a, b: 0)
+        assert_type(func, Callable[[object], str])         # typechecker believes lie about `func` type and signature
 
 
 if __name__ == '__main__':

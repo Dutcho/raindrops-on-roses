616de525b18b8294357236eb6b7766367f43991b
Olaf <Dutcho@users.noreply.github.com>
2026-08-31 22:18:52 +0200

    Improved types, a.o. with EndoFunction

--- src/favs/_internal/types.py
+++ src/favs/_internal/types.py
@@ -1,5 +1,8 @@
+"""favs.types - favourite types and `typing` functions - Olaf, 23-31 Aug 2026."""
 from collections.abc import Callable
-from typing import assert_type, cast, TYPE_CHECKING
+from typing import Annotated, Any, assert_type, cast, TYPE_CHECKING
+
+from typing_extensions import Doc
 
 
 def cast_alike[T](_exemplar: T, instance: object, /) -> T:
@@ -7,7 +10,7 @@ def cast_alike[T](_exemplar: T, instance: object, /) -> T:
 
     Equivalent to `cast(type(_exemplar), instance)`, which `mypy` 2.3.1 disapproves (though `ty` 0.0.73 allows).
 
-    >>> n = next(iter(range(1)))    # prevent typechecker from inferring `Literal`
+    >>> n = next(iter(range(1)))    # prevent typechecker from inferring `Literal[0]`
     >>> obj = cast_alike(n, 'abc')  # lie about `obj` type to typechecker
     >>> assert_type(obj, int)       # cannot validate at doctest runtime # doctest: +SKIP
     """
@@ -25,10 +28,13 @@ if TYPE_CHECKING:
 def cast_alike_callable[**PS, RT](_exemplar: Callable[PS, RT], instance: object, /) -> Callable[PS, RT]:
     """Return `instance` cast alike callable `_exemplar`, i.e. to same type and signature `Callable[PS, RT]`.
 
-    `cast_alike_callable` standardizes result type to parametrized `Callable` (for mypy), which `cast_alike` can't.
+    `cast_alike_callable` standardizes result type to parametrized `Callable` (for mypy),
+    which `cast_alike` can't do in case of overloads.
 
-    >>> func = cast_alike_callable(ascii, lambda a, b: 0)  # lie about `func` type and signature to typechecker
-    >>> assert_type(func, Callable[[object], str])         # cannot validate at doctest runtime # doctest: +SKIP
+    >>> func1 = cast_alike(ascii, lambda a, b: 0)             # lie about `func1` type/signature of simple function
+    >>> assert_type(func1, Callable[[object], str])           # cannot validate at doctest runtime # doctest: +SKIP
+    >>> func2 = cast_alike_callable(getattr, lambda a, b: 0)  # lie about `func2` type/signature of overloaded function
+    >>> assert_type(func2, Callable[[object, str], Any])      # cannot validate at doctest runtime # doctest: +SKIP
     """
     return cast(Callable[PS, RT], instance)
 
@@ -36,10 +42,19 @@ def cast_alike_callable[**PS, RT](_exemplar: Callable[PS, RT], instance: object,
 if TYPE_CHECKING:
     def _test_cast_alike_callable() -> None:
         """Execute skipped doctest at typechecking time."""
-        func = cast_alike_callable(ascii, lambda a, b: 0)
-        assert_type(func, Callable[[object], str])         # typechecker believes lie about `func` type and signature
+        func1 = cast_alike(ascii, lambda a, b: 0)
+        assert_type(func1, Callable[[object], str])           # typechecker believes lie about `func` type/signature
+        func2 = cast_alike_callable(getattr, lambda a, b: 0)
+        assert_type(func2, Callable[[object, str], Any])      # typechecker believes lie about `func` type/signature
 
 
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+type EndoFunction[T] = Annotated[Callable[[T], T],
+                                 Doc("Single-argument function with same domain and co-domain.")]
+
+
+if TYPE_CHECKING:
+    def _test_endo_function() -> None:
+        """Execute doctest equivalent at typechecking time."""
+        def test[T](_: EndoFunction[T], /) -> None: pass  # only accepts `EndoFunction`
+        def func(_: int, /) -> int: return False          # (int) -> int, i.e. `EndoFunction`
+        test(func)
